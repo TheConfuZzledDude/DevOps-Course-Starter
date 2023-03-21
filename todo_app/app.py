5dcc6b624396bf62519e510293f8bc222cb25477
Zachary Freed <zachary.freed@softwire.com>
2023-03-21 13:55:55 +0000

    Review markups

--- todo_app/app.py
+++ todo_app/app.py
@@ -10,6 +10,9 @@ from todo_app.item import Item
 
 from todo_app.view_model import ViewModel
 
+from todo_app.trello_list import TrelloList
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
@@ -17,19 +20,16 @@ def create_app():
     @app.route("/")
     def index():
         lists = [
-            (
-                cast(str,list["id"]),
-                cast(str,list["name"]),
+            TrelloList(
+                list["id"],
+                list["name"],
                 [Item.from_trello_card(card) for card in list["cards"]],
-            )
-            for list in trello_items.get_lists()
+            ) for list in trello_items.get_lists()
         ]
 
         view_model = ViewModel(lists)
 
-        return render_template(
-            "index.html", view_model=view_model
-        )
+        return render_template("index.html", view_model=view_model)
 
     @app.route("/item", methods=["POST"])
     def add_item():
@@ -38,12 +38,10 @@ def create_app():
         trello_items.add_item(title, list_id)
         return redirect(url_for("index"))
 
-
     @app.route("/item/<id>/delete", methods=["POST"])
     def delete_item(id):
         trello_items.delete_item(id)
         return redirect(url_for("index"))
-
 
     @app.route("/item/<id>/move", methods=["POST"])
     def move_item(id):
@@ -52,6 +50,7 @@ def create_app():
         return redirect(url_for("index"))
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
---
+++ todo_app/trello_list.py
@@ -0,0 +1,13 @@
+from todo_app.item import Item
+from dataclasses import dataclass
+
+@dataclass
+class TrelloList:
+    id: str
+    name: str
+    items: list[Item]
+
+    def __iter__(self):
+        return iter((self.id, self.name, self.items))
+
+
